d012acc5d7447a1a55a83c6c9e223337c0a91e8f
Ishita Chawla <84702817+ishitachawla@users.noreply.github.com>
2021-06-11 11:17:06 +0000

    update index.js

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ const github = require('@actions/github');
 const fs = require('fs');
 const filePath = core.getInput('path')
 fs.readdir(filePath, (err, files) => {
+    console.log("entered");
     if (err)
         console.log(err);
     else {
+        console.log("first if");
         const includesReadme = files.includes('Readme.md');
         if(includesReadme){
+            console.log("second if");
             $.get(filePath, function(contents){
                 var hasString = contents.includes("Example"); //boolean
                 if(!hasString)
