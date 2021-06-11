a3a8f12b7f5b1526f2b37589f148dbc3a11b0f3e
Ishita Chawla <84702817+ishitachawla@users.noreply.github.com>
2021-06-11 14:49:44 +0000

    contributions and code owners

--- index.js
+++ index.js
@@ -2,34 +2,49 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const fs = require('fs');
 const filePath = core.getInput('path')
+
 fs.readdir('./', (err, files) => {
-    console.log("entered");
-    if (err)
-        console.log(err);
-    else {
+  console.log("entered");
+  if (err)
+    console.log(err);
+  else {
+    for(let i = 0; i < files.length; i++)
+      console.log(files[i]);
+    const includesReadme = files.includes('Readme.md');
 
-        console.log("first if");
-        for(let i = 0; i < files.length; i++){
-            console.log(files[i]);
-          }
-        const includesReadme = files.includes('Readme.md');
-        console.log(includesReadme);
-        if(includesReadme){
-            console.log("Found readme");
-            fs.readFile('./Readme.md', function (err, data) {
-                if (err) throw err;
-                if(data.includes('Example')){
-                 console.log("found example");
-                }
-              });
+    if(includesReadme){
+
+      console.log("Found readme");
+      fs.readFile('./Readme.md', function (err, data) {
+      //check example
+      if(data.includes('Example'))
+        console.log("found example");
+      else
+        console.log("Example not found");
+                
+      //check contribution
+      if(data.includes('Contribution'))
+        console.log("found contribution");
+                 
+      else
+        console.log("Contribution not found");
+      });
 
 
-      }	
-        else{
-            console.log(err);
       }
-        
+    else
+      console.log("No Readme");
+
+      //code owners in .github
+
+    fs.readdir('./.github', (err, files) => {
+      const includesCodeOwners = files.includes('CODEOWNERS');
+      if(includesCodeOwners)
+        console.log("Code owners present");
+      else
+        console.log("code owners file absent");
+      })
       }
-    console.log("cool");
+    
     }
   )
