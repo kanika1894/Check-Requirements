cd67da35d149855897cfafd8c1d903a2937617a2
Ishita Chawla <84702817+ishitachawla@users.noreply.github.com>
2021-06-11 16:16:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const module = require('module');
 const fs = require('fs');
 const filePath = core.getInput('path')
 fs.readdir(filePath, (err, files) => {
