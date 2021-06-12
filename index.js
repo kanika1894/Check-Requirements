fa6847950775c89e88b7835ecf61f75bc0470e78
Ishita Chawla <84702817+ishitachawla@users.noreply.github.com>
2021-06-12 11:29:21 +0000

    add octokit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const fs = require('fs');
 const filePath = core.getInput('path')
-
+const Octokit = require("@octokit/rest");
 fs.readdir('./', (err, files) => {
   console.log("entered");
   if (err)
