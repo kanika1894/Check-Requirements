04c64316af1424036a2437671923c0912e4662af
Ishita Chawla <84702817+ishitachawla@users.noreply.github.com>
2021-06-13 08:22:56 +0000

    -

--- index.js
+++ index.js
@@ -50,9 +50,9 @@ fs.readdir('./', (err, files) => {
     //check commit protection in branch
     async function run(){
     try{
-    const repo = 'ishitachawla/Requirement-testing';
+
     const protectionOptions = {
-      url: 'https://api.github.com/repos/${repo}/branches/master/protection/required_signatures',
+      url: 'https://api.github.com/repos/ishitachawla/Requirement-testing/branches/master/protection/required_signatures',
       headers: {
         Accept: "application/vnd.github.zzzax-preview+json",
 
