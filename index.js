c0b1790a5dcf1b8293a72e3e0b41194bfa485612
Ishita Chawla <84702817+ishitachawla@users.noreply.github.com>
2021-06-13 13:24:51 +0000

    authorization

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ fs.readdir('./', (err, files) => {
       url: 'https://api.github.com/repos/ishitachawla/Requirement-testing/branches/master/protection/required_signatures',
       headers: {
         Accept: "application/vnd.github.zzzax-preview+json",
-
+        Authorization: 'Bearer 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',
       },
     };
     let protection = await request.get(protectionOptions);
