9427458151ed29c567cba26af93d1b9cc4e9cf68
Ishita Chawla <84702817+ishitachawla@users.noreply.github.com>
2021-06-14 11:44:20 +0000

    -

--- index.js
+++ index.js
@@ -118,7 +118,7 @@ fs.readdir('./', (err, files) => {
 //    const regex = /^(.*)github.com:(.*)\/(.*)\.git$/; 
     //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
     console.log(SECRET_TOKEN);
-    const result = await request('GET /repos/{owner}/{repo}/branches/{branch}/protection/required_signatures',{
+    const result = await octokit.request('GET /repos/{owner}/{repo}/branches/{branch}/protection/required_signatures',{
     owner: 'ishitachawla',
     repo: 'Requirement-testing',
     branch: 'main',
